21496a6ece4ca1f631dae87121b41da6169e0df6
MOHITKOURAV01 <mohitdas852@gmail.com>
2026-02-21 17:31:12 +0530

    Partition raw datasets into isolated training subsets variables

--- src/train_model.py
+++ src/train_model.py
@@ -16,3 +16,5 @@ def train_and_save_model():
     
     X = df['cleaned_text']
     y = df['clause_status'].astype(int)
+    
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
